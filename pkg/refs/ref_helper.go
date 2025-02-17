35ca04e86820e161124564ef907dde42bc9ae5e8
Bastian Hofmann <mail@bastianhofmann.de>
2025-02-17 10:04:16 +0100

    Remove unused code

--- pkg/refs/ref_helper.go
+++ pkg/refs/ref_helper.go
@@ -4,15 +4,3 @@ package refs
 func NewPointer[T any](value T) *T {
 	return &value
 }
-
-// DerefPointer is a generic function to dereference a pointer with a default-value fallback.
-func DerefPointer[T any](ptr *T, defaults ...T) T {
-	if ptr != nil {
-		return *ptr
-	}
-	if len(defaults) > 0 {
-		return defaults[0]
-	}
-	var empty T
-	return empty
-}
