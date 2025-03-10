c9837ba954c80f0d032cb819c681b09454d2673a
Bastian Hofmann <mail@bastianhofmann.de>
2025-03-10 11:36:58 +0100

    Clean up version flag

--- cmd/root.go
+++ cmd/root.go
@@ -8,9 +8,9 @@ import (
 )
 
 type Globals struct {
-	Debug   bool        `help:"Enable debug mode."`
-	Trace   bool        `help:"Enable trace mode."`
-	Version VersionFlag `name:"version" help:"Print version information and quit"`
+	Debug   bool             `help:"Enable debug mode."`
+	Trace   bool             `help:"Enable trace mode."`
+	Version kong.VersionFlag `name:"version" help:"Print version information and quit"`
 }
 
 type CLI struct {
@@ -19,23 +19,9 @@ type CLI struct {
 	Migrate MigrateCmd `cmd:"" help:"Migrate data to Qdrant from other sources."`
 }
 
-type VersionFlag string
-
-func (v VersionFlag) Decode(_ *kong.DecodeContext) error { return nil }
-func (v VersionFlag) IsBool() bool                       { return true }
-func (v VersionFlag) BeforeApply(app *kong.Kong, vars kong.Vars) error {
-	fmt.Println(vars["version"])
-	app.Exit(0)
-	return nil
-}
-
 func Execute(projectVersion, projectBuild string) {
 	version := fmt.Sprintf("Version: %s, Build: %s", projectVersion, projectBuild)
-	cli := CLI{
-		Globals: Globals{
-			Version: VersionFlag(version),
-		},
-	}
+	cli := CLI{}
 	ctx := kong.Parse(&cli,
 		kong.Name("migration"),
 		kong.Description("Migrate data to Qdrant from other sources."),
