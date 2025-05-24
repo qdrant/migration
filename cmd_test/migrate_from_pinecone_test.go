3d93425a001d89857a770f9251f1bd78a6337656
Anush008 <anushshetty90@gmail.com>
2025-05-24 22:24:33 +0530

    test: Sparse vectors
    Signed-off-by: Anush008 <anushshetty90@gmail.com>

--- cmd_test/migrate_from_pinecone_test.go
+++ cmd_test/migrate_from_pinecone_test.go
@@ -16,32 +16,10 @@ import (
 	"github.com/qdrant/migration/pkg/commons"
 )
 
-func TestMigrateFromPinecone(t *testing.T) {
+func TestMigrateFromPineconeDense(t *testing.T) {
 	ctx := context.Background()
 
-	qdrantContainer := qdrantContainer(ctx, t, qdrantAPIKey)
-	defer func() {
-		if err := qdrantContainer.Terminate(ctx); err != nil {
-			t.Errorf("Failed to terminate Qdrant container: %v", err)
-		}
-	}()
-
-	pineconeContainer := pineconeContainer(ctx, t)
-	defer func() {
-		if err := pineconeContainer.Terminate(ctx); err != nil {
-			t.Errorf("Failed to terminate Pinecone container: %v", err)
-		}
-	}()
-
-	pineconeHost, err := pineconeContainer.PortEndpoint(context.Background(), "5081/tcp", "http")
-	require.NoError(t, err)
-	pineconeIndexHost, err := pineconeContainer.PortEndpoint(context.Background(), "5082/tcp", "http")
-	require.NoError(t, err)
-
-	qdrantHost, err := qdrantContainer.Host(ctx)
-	require.NoError(t, err)
-	qdrantPort, err := qdrantContainer.MappedPort(ctx, qdrantPort)
-	require.NoError(t, err)
+	pineconeHost, pineconeIndexHost, qdrantHost, qdrantPort := setupContainers(t, ctx)
 
 	pineconeClient, err := pinecone.NewClient(pinecone.NewClientParams{
 		Host:   pineconeHost,
@@ -53,13 +31,15 @@ func TestMigrateFromPinecone(t *testing.T) {
 	dims := int32(dimension)
 
 	indexName := "my-serverless-index"
+	vectorType := "dense"
 
 	_, err = pineconeClient.CreateServerlessIndex(ctx, &pinecone.CreateServerlessIndexRequest{
-		Name:      indexName,
-		Dimension: &dims,
-		Metric:    &metric,
-		Cloud:     pinecone.Aws,
-		Region:    "us-east-1",
+		Name:       indexName,
+		Dimension:  &dims,
+		Metric:     &metric,
+		Cloud:      pinecone.Aws,
+		Region:     "us-east-1",
+		VectorType: &vectorType,
 	})
 	require.NoError(t, err)
 
@@ -68,41 +48,13 @@ func TestMigrateFromPinecone(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	testIDs := make([]string, totalEntries)
-	testVectors := make([][]float32, totalEntries)
-	testMetadatas := make([]map[string]interface{}, totalEntries)
-
-	for i := 0; i < totalEntries; i++ {
-		testIDs[i] = fmt.Sprintf("%d", i+1)
-
-		randomVector := make([]float32, dimension)
-		for j := range randomVector {
-			randomVector[j] = rand.Float32()
-		}
-		testVectors[i] = randomVector
-
-		testMetadatas[i] = map[string]interface{}{
-			"source": fmt.Sprintf("test%d", i+1),
-		}
-	}
-
-	vectors := make([]*pinecone.Vector, totalEntries)
-	for i := range testIDs {
-		metadata, err := structpb.NewStruct(testMetadatas[i])
-		require.NoError(t, err)
-		vectors[i] = &pinecone.Vector{
-			Id:       testIDs[i],
-			Values:   &testVectors[i],
-			Metadata: metadata,
-		}
-	}
-
+	testIDs, vectors := createTestData(t, false)
 	_, err = indexConn.UpsertVectors(ctx, vectors)
 	require.NoError(t, err)
 
 	qdrantClient, err := qdrant.NewClient(&qdrant.Config{
 		Host:   qdrantHost,
-		Port:   qdrantPort.Int(),
+		Port:   qdrantPort,
 		APIKey: qdrantAPIKey,
 	})
 	require.NoError(t, err)
@@ -116,7 +68,7 @@ func TestMigrateFromPinecone(t *testing.T) {
 			APIKey:      "qdrant-migration-test",
 		},
 		Qdrant: commons.QdrantConfig{
-			Url:        "http://" + qdrantHost + ":" + qdrantPort.Port(),
+			Url:        "http://" + qdrantHost + ":" + fmt.Sprint(qdrantPort),
 			Collection: testCollectionName,
 			APIKey:     qdrantAPIKey,
 		},
@@ -126,9 +78,8 @@ func TestMigrateFromPinecone(t *testing.T) {
 			EnsurePayloadIndexes: true,
 			OffsetsCollection:    offsetsCollectionName,
 		},
-		IdField:      idField,
-		DenseVector:  denseVectorName,
-		SparseVector: sparseVectorName,
+		IdField:     idField,
+		DenseVector: denseVectorName,
 	}
 
 	err = migrationCmd.Run(&cmd.Globals{})
@@ -152,8 +103,8 @@ func TestMigrateFromPinecone(t *testing.T) {
 			source string
 			vector []float32
 		}{
-			source: testMetadatas[i]["source"].(string),
-			vector: testVectors[i],
+			source: vectors[i].Metadata.Fields["source"].GetStringValue(),
+			vector: *vectors[i].Values,
 		}
 	}
 
@@ -167,4 +118,195 @@ func TestMigrateFromPinecone(t *testing.T) {
 		vector := point.Vectors.GetVectors().GetVectors()[denseVectorName].GetData()
 		require.Equal(t, expected.vector, vector)
 	}
+}
+
+func TestMigrateFromPineconeSparse(t *testing.T) {
+	ctx := context.Background()
+
+	pineconeHost, pineconeIndexHost, qdrantHost, qdrantPort := setupContainers(t, ctx)
+
+	pineconeClient, err := pinecone.NewClient(pinecone.NewClientParams{
+		Host:   pineconeHost,
+		ApiKey: "qdrant-migration-test",
+	})
+	require.NoError(t, err)
+
+	indexName := "my-serverless-index"
+	vectorType := "sparse"
+
+	_, err = pineconeClient.CreateServerlessIndex(ctx, &pinecone.CreateServerlessIndexRequest{
+		Name:       indexName,
+		Cloud:      pinecone.Aws,
+		Region:     "us-east-1",
+		VectorType: &vectorType,
+	})
+	require.NoError(t, err)
+
+	indexConn, err := pineconeClient.Index(pinecone.NewIndexConnParams{
+		Host: pineconeIndexHost,
+	})
+	require.NoError(t, err)
+
+	testIDs, vectors := createTestData(t, true)
+	_, err = indexConn.UpsertVectors(ctx, vectors)
+	require.NoError(t, err)
+
+	qdrantClient, err := qdrant.NewClient(&qdrant.Config{
+		Host:   qdrantHost,
+		Port:   qdrantPort,
+		APIKey: qdrantAPIKey,
+	})
+	require.NoError(t, err)
+	defer qdrantClient.Close()
+
+	migrationCmd := &cmd.MigrateFromPineconeCmd{
+		Pinecone: commons.PineconeConfig{
+			IndexName:   indexName,
+			IndexHost:   pineconeIndexHost,
+			ServiceHost: pineconeHost,
+			APIKey:      "qdrant-migration-test",
+		},
+		Qdrant: commons.QdrantConfig{
+			Url:        "http://" + qdrantHost + ":" + fmt.Sprint(qdrantPort),
+			Collection: testCollectionName,
+			APIKey:     qdrantAPIKey,
+		},
+		Migration: commons.MigrationConfig{
+			BatchSize:            batchSize,
+			CreateCollection:     true,
+			EnsurePayloadIndexes: true,
+			OffsetsCollection:    offsetsCollectionName,
+		},
+		IdField:      idField,
+		SparseVector: sparseVectorName,
+	}
+
+	err = migrationCmd.Run(&cmd.Globals{})
+	require.NoError(t, err)
+
+	points, err := qdrantClient.Scroll(ctx, &qdrant.ScrollPoints{
+		CollectionName: testCollectionName,
+		Limit:          qdrant.PtrOf(uint32(len(testIDs))),
+		WithPayload:    qdrant.NewWithPayload(true),
+		WithVectors:    qdrant.NewWithVectors(true),
+	})
+	require.NoError(t, err)
+	require.Len(t, points, len(testIDs))
+
+	expectedPoints := make(map[string]struct {
+		source string
+		vector *pinecone.SparseValues
+	})
+	for i, id := range testIDs {
+		expectedPoints[id] = struct {
+			source string
+			vector *pinecone.SparseValues
+		}{
+			source: vectors[i].Metadata.Fields["source"].GetStringValue(),
+			vector: vectors[i].SparseValues,
+		}
+	}
+
+	for _, point := range points {
+		id := point.Payload[idField].GetStringValue()
+		expected, exists := expectedPoints[id]
+		require.True(t, exists, "missing expected point with id: %s", id)
+
+		require.Equal(t, expected.source, point.Payload["source"].GetStringValue(), "source mismatch for id: %s", id)
+
+		actualSparseVec := point.Vectors.GetVectors().GetVectors()[sparseVectorName]
+		actualValues := actualSparseVec.GetData()
+		actualIndices := actualSparseVec.GetIndices().GetData()
+
+		expectedMap := make(map[uint32]float32, len(expected.vector.Indices))
+		for i, idx := range expected.vector.Indices {
+			expectedMap[idx] = expected.vector.Values[i]
+		}
+
+		actualMap := make(map[uint32]float32, len(actualIndices))
+		for i, idx := range actualIndices {
+			actualMap[idx] = actualValues[i]
+		}
+
+		require.Equal(t, len(expectedMap), len(actualMap), "sparse vector index count mismatch for id: %s", id)
+
+		for idx, expectedVal := range expectedMap {
+			actualVal, exists := actualMap[idx]
+			require.True(t, exists, "missing index %d in sparse vector for id: %s", idx, id)
+			require.Equal(t, expectedVal, actualVal, "mismatched value at index %d for id: %s", idx, id)
+		}
+	}
+
+}
+
+func setupContainers(t *testing.T, ctx context.Context) (pHost, pIndexHost, qHost string, qPort int) {
+	qdrantCont := qdrantContainer(ctx, t, qdrantAPIKey)
+	pineconeCont := pineconeContainer(ctx, t)
+
+	t.Cleanup(func() {
+		require.NoError(t, qdrantCont.Terminate(ctx))
+		require.NoError(t, pineconeCont.Terminate(ctx))
+	})
+
+	var err error
+	pHost, err = pineconeCont.PortEndpoint(ctx, "5081/tcp", "http")
+	require.NoError(t, err)
+	pIndexHost, err = pineconeCont.PortEndpoint(ctx, "5082/tcp", "http")
+	require.NoError(t, err)
+
+	qHost, err = qdrantCont.Host(ctx)
+	require.NoError(t, err)
+	mappedPort, err := qdrantCont.MappedPort(ctx, qdrantPort)
+	require.NoError(t, err)
+	qPort = mappedPort.Int()
+
+	return
+}
+
+func createTestData(t *testing.T, isSparse bool) ([]string, []*pinecone.Vector) {
+	ids := make([]string, totalEntries)
+	vectors := make([]*pinecone.Vector, totalEntries)
+
+	for i := 0; i < totalEntries; i++ {
+		ids[i] = fmt.Sprintf("%d", i+1)
+		metadata, err := structpb.NewStruct(map[string]interface{}{
+			"source": fmt.Sprintf("test%d", i+1),
+		})
+		require.NoError(t, err)
+
+		vec := &pinecone.Vector{
+			Id:       ids[i],
+			Metadata: metadata,
+		}
+
+		if isSparse {
+			sparse := &pinecone.SparseValues{
+				Indices: randIndices(5),
+				Values:  randValues(5),
+			}
+			vec.SparseValues = sparse
+		} else {
+			dense := randValues(dimension)
+			vec.Values = &dense
+		}
+
+		vectors[i] = vec
+	}
+	return ids, vectors
+}
+
+func randIndices(n int) []uint32 {
+	indices := make([]uint32, n)
+	for i := range indices {
+		indices[i] = rand.Uint32()
+	}
+	return indices
+}
+
+func randValues(n int) []float32 {
+	values := make([]float32, n)
+	for i := range values {
+		values[i] = rand.Float32()
+	}
+	return values
 }
