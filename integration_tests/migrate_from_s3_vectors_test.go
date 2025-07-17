99208b30994fc5decf11689eed9c97f0c4a86ed9
Anush <anushshetty90@gmail.com>
2025-07-17 18:55:36 +0530

    test: S3 Vectors source (#84)
    * docs: Updated README.md
    
    Signed-off-by: Anush008 <anushshetty90@gmail.com>
    
    * docs: Updated README.md
    
    * test: S3 vectors source
    
    Signed-off-by: Anush008 <anushshetty90@gmail.com>
    
    * test: Reuse bucket
    
    Signed-off-by: Anush008 <anushshetty90@gmail.com>
    
    * test: Configurable bucket name
    
    Signed-off-by: Anush008 <anushshetty90@gmail.com>
    
    ---------
    
    Signed-off-by: Anush008 <anushshetty90@gmail.com>

---
+++ integration_tests/migrate_from_s3_vectors_test.go
@@ -0,0 +1,170 @@
+package integrationtests
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3vectors"
+	"github.com/aws/aws-sdk-go-v2/service/s3vectors/document"
+	"github.com/aws/aws-sdk-go-v2/service/s3vectors/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/qdrant/go-client/qdrant"
+)
+
+func TestMigrateFromS3Vectors(t *testing.T) {
+	if os.Getenv("AWS_ACCESS_KEY_ID") == "" || os.Getenv("AWS_SECRET_ACCESS_KEY") == "" {
+		t.Skip("Skipping S3 source test. AWS credentials not set")
+	}
+
+	ctx := context.Background()
+
+	s3Bucket := os.Getenv("AWS_S3_VECTOR_BUCKET")
+	if s3Bucket == "" {
+		s3Bucket = "test-qdrant-migration-bucket"
+	}
+	t.Log("Using S3 Vectors bucket:", s3Bucket)
+
+	s3Index := "test-vector-index" + fmt.Sprintf("-%d", rand.Int())
+	t.Log("Using S3 Vectors index:", s3Index)
+
+	qdrantContainer := qdrantContainer(ctx, t, qdrantAPIKey)
+	defer func() {
+		if err := qdrantContainer.Terminate(ctx); err != nil {
+			t.Errorf("Failed to terminate Qdrant container: %v", err)
+		}
+	}()
+
+	qdrantHost, err := qdrantContainer.Host(ctx)
+	require.NoError(t, err)
+	qdrantPort, err := qdrantContainer.MappedPort(ctx, qdrantGRPCPort)
+	require.NoError(t, err)
+
+	sdkConfig, err := config.LoadDefaultConfig(ctx)
+	require.NoError(t, err)
+	s3Client := s3vectors.NewFromConfig(sdkConfig)
+
+	buckets, err := s3Client.ListVectorBuckets(ctx, &s3vectors.ListVectorBucketsInput{})
+	require.NoError(t, err)
+
+	var bucketExists bool
+	for _, bucket := range buckets.VectorBuckets {
+		if *bucket.VectorBucketName == s3Bucket {
+			bucketExists = true
+			break
+		}
+	}
+	if !bucketExists {
+		t.Logf("Creating S3 vector bucket: %s", s3Bucket)
+		_, err = s3Client.CreateVectorBucket(ctx, &s3vectors.CreateVectorBucketInput{
+			VectorBucketName: qdrant.PtrOf(s3Bucket),
+		})
+		require.NoError(t, err)
+	}
+	_, err = s3Client.CreateIndex(ctx, &s3vectors.CreateIndexInput{
+		VectorBucketName: qdrant.PtrOf(s3Bucket),
+		IndexName:        qdrant.PtrOf(s3Index),
+		Dimension:        qdrant.PtrOf(int32(dimension)),
+		DistanceMetric:   types.DistanceMetricEuclidean,
+		DataType:         types.DataTypeFloat32,
+	})
+	require.NoError(t, err)
+
+	vectors := make([]types.PutInputVector, totalEntries)
+	for i := 0; i < totalEntries; i++ {
+		key := fmt.Sprintf("vector-%d", i)
+		vecValues := make([]float32, dimension)
+		for j := 0; j < dimension; j++ {
+			vecValues[j] = rand.Float32()
+		}
+		vectors[i] = types.PutInputVector{
+			Key: qdrant.PtrOf(key),
+			Metadata: document.NewLazyDocument(map[string]interface{}{
+				"age":  i,
+				"name": fmt.Sprintf("name-%d", i),
+				"city": fmt.Sprintf("city-%d", i),
+			}),
+			Data: &types.VectorDataMemberFloat32{Value: vecValues},
+		}
+	}
+
+	_, err = s3Client.PutVectors(ctx, &s3vectors.PutVectorsInput{
+		VectorBucketName: qdrant.PtrOf(s3Bucket),
+		IndexName:        qdrant.PtrOf(s3Index),
+		Vectors:          vectors,
+	})
+	require.NoError(t, err)
+
+	qdrantClient, err := qdrant.NewClient(&qdrant.Config{
+		Host:   qdrantHost,
+		Port:   qdrantPort.Int(),
+		APIKey: qdrantAPIKey,
+	})
+	require.NoError(t, err)
+	defer qdrantClient.Close()
+
+	args := []string{
+		"s3",
+		fmt.Sprintf("--s3.bucket=%s", s3Bucket),
+		fmt.Sprintf("--s3.index=%s", s3Index),
+		fmt.Sprintf("--qdrant.url=http://%s:%s", qdrantHost, qdrantPort.Port()),
+		fmt.Sprintf("--qdrant.collection=%s", testCollectionName),
+		fmt.Sprintf("--qdrant.api-key=%s", qdrantAPIKey),
+		fmt.Sprintf("--qdrant.id-field=%s", idField),
+		fmt.Sprintf("--qdrant.dense-vector=%s", denseVectorField),
+	}
+	runMigrationBinary(t, args)
+
+	points, err := qdrantClient.Scroll(ctx, &qdrant.ScrollPoints{
+		CollectionName: testCollectionName,
+		Limit:          qdrant.PtrOf(uint32(totalEntries)),
+		WithPayload:    qdrant.NewWithPayload(true),
+		WithVectors:    qdrant.NewWithVectors(true),
+	})
+	require.NoError(t, err)
+	require.Len(t, points, totalEntries)
+
+	expected := make(map[string]struct {
+		age    int64
+		name   string
+		city   string
+		vector []float32
+	})
+	for i := 0; i < totalEntries; i++ {
+		vec := vectors[i]
+		vData := vec.Data.(*types.VectorDataMemberFloat32)
+		expected[*vec.Key] = struct {
+			age    int64
+			name   string
+			city   string
+			vector []float32
+		}{
+			age:    int64(i),
+			name:   fmt.Sprintf("name-%d", i),
+			city:   fmt.Sprintf("city-%d", i),
+			vector: vData.Value,
+		}
+	}
+
+	for _, point := range points {
+		id := point.Payload[idField].GetStringValue()
+		exp, ok := expected[id]
+		require.True(t, ok)
+		payload := point.Payload
+		require.Equal(t, exp.name, payload["name"].GetStringValue())
+		require.Equal(t, exp.city, payload["city"].GetStringValue())
+		require.Equal(t, exp.age, payload["age"].GetIntegerValue())
+		vector := point.Vectors.GetVectors().GetVectors()[denseVectorField].GetData()
+		require.Equal(t, exp.vector, vector)
+	}
+
+	_, err = s3Client.DeleteIndex(ctx, &s3vectors.DeleteIndexInput{
+		VectorBucketName: qdrant.PtrOf(s3Bucket),
+		IndexName:        qdrant.PtrOf(s3Index),
+	})
+	require.NoError(t, err)
+}
